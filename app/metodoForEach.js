0b0b476ea6d547573b179754175c5b0573a87ba5
CamilaNevesLinder <camilaneveslindner@.com>
2025-10-06 13:00:29 -0300

    refactor: finish

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,33 +1,36 @@
 const elementoParaInserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = "";
   elementoParaInserirLivros.innerHTML = "";
   listaDeLivros.forEach((livro) => {
-    //let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+    // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
     let disponibilidade =
-      livro.quantidade > 0 ? "livro_imagens" : "livro_imagens indisponivel";
+      livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
     elementoParaInserirLivros.innerHTML += `
-          
-      <div class="livro">
-      <img class="${disponibilidade}" src="${livro.imagem}"
-        alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
+        <div class="livro">
+        <img class="${disponibilidade}" src="${livro.imagem}"
+          alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
       </div>
-    </div>
-`;
+        `;
   });
 }
 
-//function verificarDisponibilidadeDoLivro(livro) {
-//if (livro.quantidade > 0) {
-//return "livro_imagens";
-//} else {
-//return "livro_imagens indisponivel";
-//}
-//}
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0 ){
+//     return 'livro__imagens'
+//   } else {
+//     return 'livros__imagens indisponivel'
+//   }
+// }
